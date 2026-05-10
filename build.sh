911fcf968e9f2d3bc238e6fc32d65a4f91ff34ca
afhuertass <afhuertass@unal.edu.co>
2026-05-10 17:59:47 +0300

    build scripts

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+# Pin to a specific Hugo version (update when needed)
+HUGO_VERSION="0.147.9"
+HUGO_URL="https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.tar.gz"
+
+curl -sL "$HUGO_URL" | tar xz hugo
+chmod +x hugo
+HUGO_ENV=production ./hugo --gc --minify
